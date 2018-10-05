0681a6c1b7278b7aa51615bc78fa9a175a7ea699
ShaniaSinha <shaniasinha109@gmail.com>
2018-10-04 21:42:03 -0400

    Added a comment

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -7,6 +7,7 @@ public class BuggyUtilClient {
                 ", last = " + "\"Flannery\"");
         System.out.println("Out: " +
         StringUtils.makePurdueUsername("Sean", "Flannery"));
+        //Made a comment, just as stated in the lab ;-; (send help i think im gonna fail CS 180.)
     }
 }
 
