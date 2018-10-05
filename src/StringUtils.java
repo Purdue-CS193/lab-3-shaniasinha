12012035f46f6e7f90787464f045c99f467cc3b6
ShaniaSinha <shaniasinha109@gmail.com>
2018-10-04 22:09:25 -0400

    in MathUtils: 1. changed line 6 such that variables dividend and divisor are of type double 2. Also for loop in line 31 runs from index 0 to index (exp-2) in StringUtils: created a new empty string to save the shortened 8 characters long string. Also if last name is shorter than 8 characters, it outputs a username accordingly.

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -12,19 +12,25 @@ public class StringUtils {
             Ex: firstName = "Sean", lastName = "Flannery"
                     --> res = "SFlannery"
          */
-        String res = firstName.substring(0,1) + lastName;
+        String s;
+        String f = firstName.toLowerCase();
+        String l = lastName.toLowerCase();
+        String res = f.substring(0,1) + l;
         /* Shorten the string to 8 characters
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
          */
-        res.substring(0, 8);
+        if (res.length() > 8) {
+            s = res.substring(0, 8);
         /* Convert the name to lower-case
             Ex: res = "SFlanner"
                     --> res = "sflanner"
          */
-        res.toLowerCase();
+        } else {
+            s = res;
+        }
         /* return the result of our calculation */
-        return res;
+        return s;
     }
 
     /**
@@ -36,6 +42,6 @@ public class StringUtils {
      */
     public static String replaceStudentUsername(String text, String username) {
         /* Replace every instance of the username with proper message */
-        return text.replaceAll(username, "[DATA EXPUNDGED]");
+        return text.replaceAll(username, "[DATA EXPUNGED]");
     }
 }
